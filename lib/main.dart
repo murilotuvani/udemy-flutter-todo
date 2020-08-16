9f7e7a76d8d62d3e7c10bd759b3500bd34dab5d3
Murilo Tuvani <murilo@autogeral.com.br>
2020-08-16 00:19:00 -0300

    Excluindo e desfazendo exclusão da lista

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,120 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _doDoController = TextEditingController();
+
   List _toDoList = [];
+  Map<String, dynamic> _lastRemove;
+  int _lastRemovedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _doDoController.text;
+      newToDo["ok"] = false;
+      print("Adding to TO-DO list ... ${newToDo}");
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Tarefas - TO-DO"),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _doDoController,
+                    decoration: InputDecoration(
+                        labelText: "Nova tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  )),
+                  RaisedButton(
+                    color: Colors.blueAccent,
+                    child: Text("Add"),
+                    textColor: Colors.white,
+                    onPressed: _addToDo,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
+            )
+          ],
+        ));
+  }
+
+  //Widget buildItem(context, index) {
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(Icons.delete, color: Colors.white)),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error)),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemove = Map.from(_toDoList[index]);
+          _lastRemovedIndex = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemove["title"]}\" removida!"),
+            action: SnackBarAction(label: "Desfazer", onPressed: () {
+              setState(() {
+                _toDoList.insert(_lastRemovedIndex, _lastRemove);
+                _saveData();
+              });
+            }),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
   }
 
   Future<File> _getFile() async {
@@ -27,8 +136,17 @@ class _HomeState extends State<Home> {
   }
 
   Future<File> _saveData() async {
-    String data = json.econde(_toDoList);
+    String data = json.encode(_toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
